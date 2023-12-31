44ddbc5fe42dc5b5906aa8984c3df404e952e93c
Steve Hannah <shannah@weblite.ca>
2023-12-31 12:18:22 -0800

    Changed background to yellow

--- src/main/java/com/github/shannah/testswing11/TestSwing11.java
+++ src/main/java/com/github/shannah/testswing11/TestSwing11.java
@@ -30,7 +30,7 @@ public class TestSwing11 extends JFrame {
         stopButton.addActionListener(e -> timer.stop());
 
         animatedPanel = new AnimatedPanel();
-        animatedPanel.setBackground(Color.CYAN);
+        animatedPanel.setBackground(Color.YELLOW);
         setLayout(new BorderLayout());
         add(animatedPanel, BorderLayout.CENTER);
         add(startButton, BorderLayout.NORTH);
