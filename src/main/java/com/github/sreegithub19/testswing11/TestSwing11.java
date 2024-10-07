85941caa4c4ae35d1d33a7b4db518f2913e9be9f
Sreedhar K <sreedhar.k@rakuten.com>
2024-10-07 18:59:44 +0530

    C

---
+++ src/main/java/com/github/sreegithub19/testswing11/TestSwing11.java
@@ -0,0 +1,56 @@
+package com.github.sreegithub19.testswing11;
+import javax.swing.*;
+import java.awt.*;
+
+public class TestSwing11 extends JFrame {
+
+    private Timer timer;
+    private int x, y, xDelta = 2, yDelta = 2;
+    private JButton startButton, stopButton;
+    private AnimatedPanel animatedPanel;
+
+    public TestSwing11() {
+        x = y = 100;
+        timer = new Timer(10, e -> {
+            x += xDelta;
+            y += yDelta;
+
+            if (x + "Hello jDeploy".length() * 7 >= animatedPanel.getWidth() || x < 0) xDelta *= -1;
+            if (y + 15 >= animatedPanel.getHeight() || y < 0) yDelta *= -1;
+
+            animatedPanel.repaint();
+        });
+
+        startButton = new JButton("Start");
+        startButton.addActionListener(e -> timer.start());
+
+        stopButton = new JButton("Stop");
+        stopButton.addActionListener(e -> timer.stop());
+
+        animatedPanel = new AnimatedPanel();
+        animatedPanel.setBackground(Color.BLUE);
+        setLayout(new BorderLayout());
+        add(animatedPanel, BorderLayout.CENTER);
+        add(startButton, BorderLayout.NORTH);
+        add(stopButton, BorderLayout.SOUTH);
+    }
+
+    class AnimatedPanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.drawString("""
+                <h1>Hello j_Deploy 17</h1>
+                         """, x, y);
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            TestSwing11 frame = new TestSwing11();
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(400, 400);
+            frame.setVisible(true);
+        });
+    }
+}
